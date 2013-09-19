d018842f13663aa5d647d9f1771c0d8c74cd1e0c
Mike <mike@mike-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-09-19 14:32:29 -0600

    Need timing files

--- io_test.cpp
+++ io_test.cpp
@@ -3,30 +3,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "timing.h"
 
 using namespace std;
 
 int main()
 {
-  string io_test = "./io_text ";
+  string io_text = "./io_text ";
   string io_hdf = "./io_hdf ";
   string python_matrix = "python generate_matrix.py";
 
   string test_size[7] = {"10", "50", "100", "500", "1000", "2000", "5000"};
 
-  // Run io_text for the test array dimensions
+  // Run io_text and io_hdf for the test array dimensions
   for(int i = 0; i < 7; i++)
     {
       string N = test_size[i];
+      double start, stop;
       system((python_matrix + " -m " + N + " -n " + N + " -f matrix." + N + "." + N).c_str());
-      /* FILE* output = popen((, "r");
-      char buffer[128];
-      std::string result = "";
-      while(!feof(output)) {
-	    if(fgets(buffer, 128, output) != NULL)
-    		result += buffer;
-	    }
-	results[i-1] = result; */
+      
+      get_walltime(&start);
+      system((io_text + "matrix." + N + "." + N).c_str());
+      get_walltime(&stop);
+
+      cout << stop - start << endl;
+      
+	    
     }
 
  // ofstream out("scaling.csv");  
